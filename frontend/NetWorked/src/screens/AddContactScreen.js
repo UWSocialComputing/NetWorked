6d4930e3fb6a7307f54a46e4720f592761bbed31
jbalinas <108829766+jbalinas@users.noreply.github.com>
2024-03-03 18:05:21 -0800

    Connecting addContactScreen to firestore to save contacts

--- frontend/NetWorked/src/screens/AddContactScreen.js
+++ frontend/NetWorked/src/screens/AddContactScreen.js
@@ -12,6 +12,7 @@ import {
 } from 'react-native';
 import styles from '../config/styles'; // Adjust the import path according to your project structure
 import colors from '../config/colors'; // Adjust the import path according to your project structure
+import firestore from '@react-native-firebase/firestore';
 
 const AddContactScreen = ({navigation, route}) => {
   const [name, setName] = useState('');
@@ -32,11 +33,36 @@ const AddContactScreen = ({navigation, route}) => {
   ); // Determine if editing based on passed contact
 
   const handleSave = () => {
-    // Implement save functionality here
-      if (notificationsEnabled) {
-          // Logic to schedule notifications
+    // Implement save functionality here, add logic to schedule notifications
+      // Validate form data before saving
+      if (name.trim() === '') {
+          Alert.alert('Error, Name field cannot be empty.');
+          return;
       }
-    Alert.alert('Save Contact', 'Contact saved successfully!');
+
+      // Contact object constructor
+      const contactData = {
+          name: name,
+          phone: phone,
+          email: email,
+          relationship: relationship,
+          communication: communicationType,
+          frequency: communicationFrequency,
+          notifications: notificationsEnabled
+      };
+
+      firestore()
+          .collection('Contacts')
+          .add(contactData)
+          .then(() => {
+              // Successful save
+              Alert.alert('Save Contact', 'Contact saved successfully!');
+              navigation.goBack();
+          })
+          .catch(error => {
+              console.error('Error adding contact: ', error);
+              Alert.alert('Error', 'Failed to save contact.');
+          });
   };
 
   const handleCancel = () => {
